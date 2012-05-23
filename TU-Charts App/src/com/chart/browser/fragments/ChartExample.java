3fece4702697f7ad209f2f2d3eba720347356255
Didac <didac.montero.mendez@gmail.com>
2012-05-23 12:52:01 +0200

    ChartFragment: Added a fake Chart, using achartengine library.

---
+++ TU-Charts App/src/com/chart/browser/fragments/ChartExample.java
@@ -0,0 +1,107 @@
+package com.chart.browser.fragments;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import com.actionbarsherlock.R;
+import com.chart.pojos.ChartEntry;
+import com.chart.pojos.Point;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
+
+public class ChartExample {
+
+	private static final String DATE_FORMAT = "dd/MM/yyyy";
+
+	public GraphicalView getView(Context context, List<Point> results, ChartEntry chartEntry) {
+
+		int[] colors = new int[] { Color.BLUE };
+		PointStyle[] styles = new PointStyle[] { PointStyle.POINT};
+		XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
+
+		Point max = Collections.max(results);
+		Point min = Collections.min(results);
+		System.out.println("max=" + max + " min="+min);
+		System.out.println(results);
+
+		setChartSettings(renderer, chartEntry.name,"Dates", "Values", results.get(0)
+				.date.getTime(), results.get(results.size() - 1)
+						.date.getTime(), min.value, max.value, Color.DKGRAY, Color.BLACK, context);
+		renderer.setXLabels(5);
+		renderer.setYLabels(10);
+		SimpleSeriesRenderer seriesRenderer = renderer.getSeriesRendererAt(0);
+		seriesRenderer.setDisplayChartValues(true);
+		return ChartFactory.getTimeChartView(context,
+				buildDateDataset(chartEntry.name, results), renderer, DATE_FORMAT);
+	}
+
+	protected void setChartSettings(XYMultipleSeriesRenderer renderer,
+			String title, String xTitle, String yTitle, double xMin,
+			double xMax, double yMin, double yMax, int axesColor,
+			int labelsColor, Context context) {
+		renderer.setChartTitle(title);
+		renderer.setXTitle(xTitle);
+		renderer.setYTitle(yTitle);
+		renderer.setXAxisMin(xMin);
+		renderer.setXAxisMax(xMax);
+		renderer.setYAxisMin(yMin);
+		renderer.setYAxisMax(yMax);
+		renderer.setAxesColor(axesColor);
+		renderer.setLabelsColor(labelsColor);
+		renderer.setYLabelsColor(0, Color.DKGRAY);
+//		renderer.setXLabelsColor(Color.GRAY);
+
+	    renderer.setYLabelsAlign(Align.RIGHT);
+		renderer.setApplyBackgroundColor(true);
+		int holo_bg= context.getResources().getColor(R.color.abs__background_holo_light);
+		renderer.setBackgroundColor(holo_bg);
+		renderer.setMarginsColor(holo_bg);
+	}
+
+	protected XYMultipleSeriesRenderer buildRenderer(int[] colors,
+			PointStyle[] styles) {
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		setRendererProperties(renderer, colors, styles);
+		return renderer;
+	}
+
+	protected XYMultipleSeriesDataset buildDateDataset(String title,
+			List<Point> results) {
+		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		TimeSeries series = new TimeSeries(title);
+		for (Point result : results) {
+			series.add(result.date, result.value);
+		}
+		dataset.addSeries(series);
+		return dataset;
+	}
+
+	protected void setRendererProperties(XYMultipleSeriesRenderer renderer, int[] colors,
+			PointStyle[] styles) {
+		renderer.setAxisTitleTextSize(16);
+		renderer.setChartTitleTextSize(20);
+		renderer.setLabelsTextSize(15);
+		renderer.setLegendTextSize(15);
+		renderer.setPointSize(5f);
+		renderer.setMargins(new int[] { 20, 30, 15, 20 });
+		int length = colors.length;
+		for (int i = 0; i < length; i++) {
+			XYSeriesRenderer r = new XYSeriesRenderer();
+			r.setColor(colors[i]);
+			r.setPointStyle(styles[i]);
+			renderer.addSeriesRenderer(r);
+		}
+	}
+}
+
--- TU-Charts App/src/com/chart/browser/fragments/ChartFragment.java
+++ TU-Charts App/src/com/chart/browser/fragments/ChartFragment.java
@@ -1,5 +1,11 @@
 package com.chart.browser.fragments;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.achartengine.GraphicalView;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,6 +13,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
@@ -15,16 +23,22 @@ import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.chart.R;
 import com.chart.pojos.ChartEntry;
+import com.chart.pojos.Point;
 
 public class ChartFragment extends SherlockFragment{
 	private ChartEntry chart;
 	private FragmentManager fm;
+	private ChartExample chartBuilder;
+	GraphicalView mChart;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		System.out.println("Original");
 		super.onCreate(savedInstanceState);
 		chart = (ChartEntry) (getArguments() != null ? getArguments().getParcelable("chart") : null);
 		fm = getActivity().getSupportFragmentManager();
+		chartBuilder =  new ChartExample();
+
 		setHasOptionsMenu(true);
 	}
 
@@ -32,8 +46,10 @@ public class ChartFragment extends SherlockFragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.hello_world, container, false);
-		View tv = v.findViewById(R.id.text);
-		((TextView)tv).setText("Fragment where it will be included the Chart: " + chart.name);
+		LinearLayout layout= (LinearLayout) v.findViewById(R.id.chart);
+		mChart= chartBuilder.getView(v.getContext(), getPoints(),chart);
+		layout.addView(mChart, new LayoutParams(LayoutParams.FILL_PARENT,
+		          LayoutParams.FILL_PARENT));
 		return v;
 	}
 
@@ -46,14 +62,56 @@ public class ChartFragment extends SherlockFragment{
 	}
 
 	@Override
+	public void onResume() {
+		super.onResume();
+		System.out.println("BEFORE");
+		if (chartBuilder != null)
+			mChart.repaint();
+		System.out.println("AFTER");
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Fragment frag=ChartActivity.InfoFragment.newInstance(chart);
 		FragmentTransaction ft= fm.beginTransaction();
 		ft.setCustomAnimations(R.anim.fragment_slide_left_enter,R.anim.fragment_slide_left_exit,R.anim.fragment_slide_right_enter,R.anim.fragment_slide_right_exit);
-//		ft.remove(this);
-//		ft.add(android.R.id.content, frag, "Info");
+		//		ft.remove(this);
+		//		ft.add(android.R.id.content, frag, "Info");
 		ft.replace(android.R.id.content, frag, "Info");
 		ft.addToBackStack("Chart").commit();
 		return true;
 	}
+
+	private List<Point> getPoints() {
+		Calendar c = Calendar.getInstance();
+		List<Point> results = new ArrayList<Point>();
+		c.set(2012, 9, 1);
+		results.add(new Point(c.getTime(), 8.8));
+		c.clear();
+		c.set(2012, 9, 8);
+		results.add(new Point(c.getTime(), 9.0));
+		c.clear();
+		c.set(2012, 9, 15);
+		results.add(new Point(c.getTime(), 10.0));
+		c.clear();
+		c.set(2012, 9, 22);
+		results.add(new Point(c.getTime(), 9.5));
+		c.clear();
+		c.set(2012, 9, 23);
+		results.add(new Point(c.getTime(), 11.0));
+		c.clear();
+		c.set(2012, 9, 25);
+		results.add(new Point(c.getTime(), 10.8));
+		c.clear();
+		c.set(2012, 9, 27);
+		results.add(new Point(c.getTime(), 12.5));
+		c.clear();
+		c.set(2012, 9, 28);
+		results.add(new Point(c.getTime(), 6.5));
+		c.clear();
+		c.set(2012, 9, 29);
+		results.add(new Point(c.getTime(), 9.5));
+		return results;
+	}
+
 }
